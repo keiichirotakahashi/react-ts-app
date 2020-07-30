ed34f9767506c4fd98265e850e113d12c7777794
keiichirotakahashi <k16takahashi125@gmail.com>
2020-07-30 09:53:46 +0900

    Add a UserTable Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import UserTable from './tables/UserTable';
 
 const App: React.FC = () => {
+  type User = {
+    id: number;
+    name: string;
+    username: string;
+  };
+
+  const usersData: User[] = [
+    { id: 1, name: 'たぬきち', username: 'tanutanu' },
+    { id: 2, name: 'まめきち', username: 'mamekko55' },
+    { id: 3, name: 'つぶきち', username: 'tsubumaru' },
+  ];
+
+  const [users, setUsers] = useState(usersData);
+
   return (
     <div className="container">
-      <h1>CRUD App with Hooks</h1>
+      <h1>ユーザーCRUDアプリ</h1>
       <div className="flex-row">
         <div className="flex-large">
-          <h2>Add user</h2>
+          <h2>ユーザーを追加する</h2>
         </div>
         <div className="flex-large">
-          <h2>View users</h2>
+          <h2>ユーザー一覧</h2>
+          <UserTable users={users} />
         </div>
       </div>
     </div>
